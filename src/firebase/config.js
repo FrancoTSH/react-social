7b76c47e3fb3e220fee5739e51b7aa0cc3532d29
Franco <58120780+FrancoTSH@users.noreply.github.com>
2021-05-16 12:19:47 -0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 import 'firebase/storage';
 
 const config = {
-  apiKey: 'AIzaSyBR3B24TKXflq38A5ce879C76WBuv5XRAA',
-  authDomain: 'react-project-62afa.firebaseapp.com',
-  projectId: 'react-project-62afa',
-  storageBucket: 'react-project-62afa.appspot.com',
-  messagingSenderId: '1044991478389',
-  appId: '1:1044991478389:web:a8afc5de81b3890b58463c'
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
 };
 
 const firebase = Firebase.initializeApp(config);
